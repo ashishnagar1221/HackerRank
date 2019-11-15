63d8465f791a2b3bff4a03b239b30ad4f2c02d51
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-11-15 09:46:43 +0530

    Bon Appétit | HackerRank

---
+++ bonAppetit.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {
+    var b_actual = 0
+    var result
+    for(var i in bill){
+        if( i == k)
+            continue
+        b_actual+=bill[i]
+    }
+    b_actual/=2
+    result = b - b_actual
+    if(result <= 0)
+    console.log("Bon Appetit")
+    else console.log(result)
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
