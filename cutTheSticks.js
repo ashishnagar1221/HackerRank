954ecffd74e2f7907611545b7b47b1c9510d7bdd
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-11-17 10:10:31 +0530

    Cut the Sticks | HackerRank

---
+++ cutTheSticks.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the cutTheSticks function below.
+function cutTheSticks(arr) {
+    let l = [];
+    arr.sort(function (a, b) {
+        return b - a;
+    });
+    while(arr.length!=0) {
+        l.push(arr.length);
+        arr = arr.map(x => x - arr[arr.length - 1]);
+        
+        arr = arr.filter(x => x != 0);
+        
+    }
+    return l;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = cutTheSticks(arr);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
