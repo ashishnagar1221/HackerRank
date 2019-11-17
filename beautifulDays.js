f90df9d7a2b2a062171a27529f71faa2ca555899
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-11-17 10:20:50 +0530

    Beautiful Days at the Movies | HackerRank

---
+++ beautifulDays.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the beautifulDays function below.
+function beautifulDays(i, j, k) {
+    var arr = []
+    var count = 0
+    for(i;i<=j;i++){
+        //console.log(i)
+        var x = parseInt(i.toString(10).split('').reverse().join(''))
+        arr.push(Math.abs( i - x)/k)
+    }
+    for(var i in arr){
+        if(arr[i] % 1 === 0)
+            count++
+    }
+return count
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const ijk = readLine().split(' ');
+
+    const i = parseInt(ijk[0], 10);
+
+    const j = parseInt(ijk[1], 10);
+
+    const k = parseInt(ijk[2], 10);
+
+    let result = beautifulDays(i, j, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
