06a649d765298ed33bd7822e38c03a7202971517
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-12-07 19:03:16 +0530

    Forming a Magic Square | HackerRank

---
+++ formingMagicSquare.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the formingMagicSquare function below.
+function formingMagicSquare(s) {
+      //Enter your code here
+    
+    var minCost = 81;
+    var magiccompare = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]], 
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],  
+    ];
+                                         
+    for(var k = 0; k < magiccompare.length; k++){
+        var cCost = 0;
+        for(var i = 0; i < magiccompare[k].length; i++){
+            for(var j = 0; j < magiccompare[k].length; j++){
+                cCost += Math.abs( s[i][j] - magiccompare[k][i][j] )
+            }    
+        }
+        if(cCost < minCost){
+            minCost = cCost;
+        }
+    }
+    return minCost;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let s = Array(3);
+
+    for (let i = 0; i < 3; i++) {
+        s[i] = readLine().split(' ').map(sTemp => parseInt(sTemp, 10));
+    }
+
+    const result = formingMagicSquare(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
