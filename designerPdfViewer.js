10a36264819220ae70008a00f7bd15145273a4c7
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-12-07 19:07:34 +0530

    Designer PDF Viewer | HackerRank

---
+++ designerPdfViewer.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the designerPdfViewer function below.
+function designerPdfViewer(h, word) {
+
+   var coch = []
+    var length = []
+    var result
+    var len = word.length;
+    for(var i=0;i<len;i++){
+      coch.push(word.charCodeAt(i)-97);
+    }   
+    for(var i in coch){
+        length.push(h[coch[i]])
+    }
+        
+    result = Math.max(...length) * len 
+    return result
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    let result = designerPdfViewer(h, word);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
