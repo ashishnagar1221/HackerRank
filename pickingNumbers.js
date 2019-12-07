900eaa1bc23b1c05f4f365b20caa7d7bbde68342
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-12-07 19:05:38 +0530

    Picking Numbers | HackerRank

---
+++ pickingNumbers.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    // Write your code here
+  var res = 0
+    for(var i=0;i<100;i++){
+        var num1 = i
+        var num2 = i+1
+        var curRes = 0
+        for(var j = 0;j <a.length;j++){
+            if(a[j] === num1 || a[j] === num2)
+            curRes++
+        }
+        if(curRes > res)
+            res = curRes
+    }
+return res
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
