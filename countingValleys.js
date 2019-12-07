c9d1413d870303558459441c8a8443fdc1e21967
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-12-07 18:59:46 +0530

    Counting Valleys | HackerRank

---
+++ countingValleys.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    var stack = 0
+    var stackCount = 0
+    var arr = s.split('')
+    for(var i =0;i<arr.length;i++){
+        if(arr[i] === 'U'){
+            if(stackCount === -1)
+                stack++
+            stackCount++
+        }else{
+            stackCount --
+        }
+    }return stack
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
