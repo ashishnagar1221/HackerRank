93c97ed020116352b6148505d2a50621c026cbe9
ashishnagar1221 <ashishnagar1221@gmail.com>
2019-12-19 00:10:31 +0530

    Climbing Leaderboard | HackerRank

---
+++ climbingLeaderboard.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the climbingLeaderboard function below.
+function climbingLeaderboard(scores, alice) {
+
+      var result = []
+    var array = Array.from(new Set(scores).values());
+    console.log(array)
+    var j = array.length - 1
+for(var i in alice){
+    while(j >= 0 && array[j] <= alice[i]) --j
+        result.push(j+2)
+}
+return result
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const scoresCount = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const aliceCount = parseInt(readLine(), 10);
+
+    const alice = readLine().split(' ').map(aliceTemp => parseInt(aliceTemp, 10));
+
+    let result = climbingLeaderboard(scores, alice);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
